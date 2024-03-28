53abc076a3e1d03517309921da23619eb48e175e
foxstudent106814 <nhoroshenko111@gmail.com>
2024-03-28 11:50:05 +0200

    Task3

---
+++ src/main/java/org/example/homework3/Task3.java
@@ -0,0 +1,14 @@
+package org.example.homework3;
+
+/**
+ * Program to Convert long to int
+ */
+public class Task3 {
+    public static void main(String[] args) {
+        long longValue = 123456789012L;
+
+        int intValue = (int) longValue;
+        System.out.println("Original long value: " + longValue);
+        System.out.println("Converted int value: " + intValue);
+    }
+}
