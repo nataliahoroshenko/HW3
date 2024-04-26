cf3e55f74f0b1a3a0646cb377883d41832dc65c3
natgoro <retankan@gmail.com>
2024-04-26 14:49:58 +0300

    homework8

---
+++ src/main/java/org/example/homework8/printer/Printer.java
@@ -0,0 +1,47 @@
+package org.example.homework8.printer;
+
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private final boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        if (tonerLevel > -1 && tonerLevel <= 100) {
+            this.tonerLevel = tonerLevel;
+        } else {
+            this.tonerLevel = -1;
+        }
+
+        this.pagesPrinted = 0;
+
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount) {
+        if (tonerAmount > 0 && tonerAmount <= 100) {
+            if (this.tonerLevel + tonerAmount > 100) {
+                return -1;
+            } else {
+                this.tonerLevel += tonerAmount;
+                return this.tonerLevel;
+            }
+        } else {
+            return -1;
+        }
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+
+        if (this.duplex) {
+            pagesToPrint = (pages + 1) / 2; // Round up for odd pages
+        }
+
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
+    public int getPagesPrinted() {
+        return this.pagesPrinted;
+    }
+}
